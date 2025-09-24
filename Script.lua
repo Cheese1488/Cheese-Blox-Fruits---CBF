6126641079b5d70c52a5025b18600a8601504a1c
Serochec <stepakazlov@gmail.com>
2025-09-24 15:15:14 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,6 @@
-print("test")
-game.Players.localPlayer:from
+local LocalPlayer = game:GetService("Players").LocalPlayer  
+local Character = LocalPlayer.Character  
+if not Character or not Character.Parent then  
+    Character = LocalPlayer.CharacterAdded:Wait()  
+end  
+Character:MoveTo(-5, 3, 7) 
